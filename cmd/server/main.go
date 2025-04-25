72ae0a69b95a9e037d324414c3d94812e8bb87cc
Nerdy Gedoni <gedoniani@gmail.com>
2025-04-25 12:39:54 +0100

    refactor: reorganize imports and enhance route definitions in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,36 @@
 package main
 
-import (  
-  "github.com/gin-gonic/gin"
-  "go-repl/internal/handler"
+import (
+	"go-repl/internal/handler"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 
 func main(){
 	router := gin.Default()
 
+	// Welcome route
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the Go Playground REPL API 🚀",
+		})
+	})
+
+	// Health check route
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
 	// Set up the routes and attach the handlers
 	router.POST("/run", handler.RunCode)
 
 	//start the server on port 8080
 	router.Run(":8080")
+
+	
+
 }
